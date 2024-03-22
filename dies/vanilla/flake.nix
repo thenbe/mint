ae98911502c8c398ebf5fda4b3b45493711afebb
Naser Aleisa <aleisa.naser@gmail.com>
2024-03-22 14:19:26 +0300

    feat: vanilla coin

---
+++ dies/vanilla/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Vanilla Coin";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        # foo = pkgs.callPackage ./foo/default.nix { };
+      in
+      {
+        packages = {
+          # inherit foo;
+          # bar = bar.outputs.packages.${system}.default;
+        };
+
+        apps = {
+          # foo = { program = "${foo}/bin/foo.sh"; type = "app"; };
+        };
+
+        devShells = {
+          # foo = pkgs.mkShell { packages = [ foo pkgs.cmake ]; };
+          default = pkgs.mkShell {
+            packages = [
+              # foo
+              # pkgs.cmake
+            ];
+            shellHook = ''
+              set -eu
+              # echo hello
+            '';
+          };
+        };
+      }
+    );
+}
