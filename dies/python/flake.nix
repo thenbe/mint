b43b01c41c870f2e6ae2f6d2774d195bfa0d00b2
Naser Aleisa <aleisa.naser@gmail.com>
2024-04-21 06:51:00 +0300

    feat: python coin

---
+++ dies/python/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Python Coin";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, }:
+    let
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default =
+          let
+            pypkgs = pkgs.python311Packages;
+          in
+          pkgs.mkShell {
+            name = "python-env";
+            venvDir = "./.venv";
+            packages = [
+              pypkgs.python
+              pypkgs.venvShellHook
+              # pkgs.foo
+            ];
+            # Run this command, only after creating the virtual environment
+            postVenvCreation = ''
+              unset SOURCE_DATE_EPOCH
+              # pip install -r requirements.txt
+            '';
+
+            # Now we can execute any commands within the virtual environment.
+            # This is optional and can be left out to run pip manually.
+            postShellHook = ''
+              set -eu
+              # allow pip to install wheels
+              unset SOURCE_DATE_EPOCH
+            '';
+          };
+      });
+    };
+}
